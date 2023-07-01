5a3a6fe56a4cf2910c5007f58a5d6055b6d430c5
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-07-01 12:02:13 +0100

    Foundation

---
+++ app.py
@@ -0,0 +1,18 @@
+notes = []
+
+
+def addNote():
+    pass
+
+
+def printNotes():
+    pass
+
+
+def deleteNote():
+    pass
+
+
+while True:
+    print("TODOLIST APP")
+    print("Select What you want to do\n 1. Add a Todo\n 2. Delete a Todo\n 3. Print all Todo's")

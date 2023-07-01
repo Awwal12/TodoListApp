a0efbff495f3c6af6f36a3bfef4fb364d663b313
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-07-01 12:19:47 +0100

    added .gitignore file and added exception handling for when choosing options

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+class NotPositiveError(UserWarning):
+    pass
+
+
 notes = []
 
 
@@ -16,3 +20,21 @@ def deleteNote():
 while True:
     print("TODOLIST APP")
     print("Select What you want to do\n 1. Add a Todo\n 2. Delete a Todo\n 3. Print all Todo's")
+    try:
+        response = int(input('Input a number from 1-3: '))
+        if response <= 0:
+            raise NotPositiveError
+        elif response == 1:
+            addNote()
+        elif response == 2:
+            deleteNote()
+        elif response == 3:
+            printNotes()
+        else:
+            raise IndexError
+    except ValueError:
+        print("please input a numeric value")
+    except NotPositiveError:
+        print("Please input a positive number")
+    except IndexError:
+        print("Index out of range")
